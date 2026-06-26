a533065417fa6f8e337d209dc7c5cc4af9bc27d9
Alexis Cruz <jr.alexis.cruz.2003@gmail.com>
2026-06-25 21:13:38 -0300

    chore: setup and initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  eslintConfigPrettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-console": "warn",
+    },
+  },
+]);
+
+export default eslintConfig;
